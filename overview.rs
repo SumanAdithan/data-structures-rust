62a1a36a8a87a655b0d8ac64820f44bf57bff95e
SumanAdithan <sumanadithang34@gmail.com>
2025-09-28 23:27:34 +0530

    feat: add overview for data structures

---
+++ overview.rs
@@ -0,0 +1,31 @@
+// What ia a data structure?
+// A data structure is a way to store and organize data so that it can be used efficiently
+// A data structure is a collection of data values, the relationships among them, and the functions or operations that can be applied to that data
+
+// Why data structures?
+// Almost every applications we build involves data that is modelled in a certain way
+// To efficiently manage that data, you need data structures
+// Imagine walking into a library only to find out there is no efficient organization of the books and they are stored randomly
+
+// The difference between a function taking a few milliseconds vs a few seconds or even minutes come down to the selection of right data structure
+// Data structures helps you solve problems in a more efficient way, both in terms of time and memory
+// data structures also helps you gain a more profound understanding of things you're already aware of
+//  - DOM - Tree data structure
+//  - Browser back and Forward - Stack data structure
+//  - OS job scheduling - Queue data structure
+
+// Build in data structures
+//  - Arrays
+//  - Vectors
+//  - Tuples
+//  - HashSet
+//  - HashMap
+
+// Custom data structures
+//  - Stacks
+//  - Queues
+//  - Circular queues
+//  - Linked list
+//  - Hash tables
+//  - Trees
+//  - Graphs
